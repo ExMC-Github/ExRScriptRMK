429aa06efeeedfe717696d6b0aed23bf99dc9989
ExMC-Github <146938874+ExMC-Github@users.noreply.github.com>
2025-12-23 17:14:09 +0800

    v0.2.4_include

---
+++ include/include.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <cstdio>
+#include <string>
--- include/krnln.h
+++ include/krnln.h
@@ -10,40 +10,41 @@
 // -----------------------
 // 系统核心支持库
 // File: krnln.h
-// File Version: 0.1.2
+// File Version: 0.1.5
 // -----------------------
 #ifndef KRNLN_H
 #define KRNLN_H
-#include <windows.h>
-#include <iostream>
-#include <string>
-#include <stdio.h>
+#include "include.h"
 using std::string;
 using std::to_string;
 extern string runpath;
 extern string exeName;
 
-void msgbox(string content, string title = "") {
-    MessageBox(NULL,content.c_str(),title.c_str(), MB_OK | 0);
+int MsgBox(const char* content, const char* title = "") {
+    return MessageBoxA(NULL,content,title, MB_OK | 0);
 };
 
-void msgbox_error(string content, string title = "") {
-    MessageBox(NULL,content.c_str(),title.c_str(), MB_OK | 16);
+int MsgBox_Error(const char* content, const char* title = "") {
+    return MessageBoxA(NULL,content,title, MB_OK | 16);
 };
 
-void msgbox_info(string content, string title = "") {
-    MessageBox(NULL,content.c_str(),title.c_str(), MB_OK | MB_ICONINFORMATION);
+int MsgBox_Info(const char* content, const char* title = "") {
+    return MessageBoxA(NULL,content,title, MB_OK | MB_ICONINFORMATION);
 };
 
-void msgbox_ask(string content, string title = "") {
-    MessageBox(NULL,content.c_str(),title.c_str(), MB_OK | 32);
+int MsgBox_Ask(const char* content, const char* title = "") {
+    return MessageBoxA(NULL,content,title, MB_OK | 32);
 };
 
-void msgbox_warn(string content, string title = "") {
-    MessageBox(NULL,content.c_str(),title.c_str(), MB_OK | 48);
+int MsgBox_Warn(const char* content, const char* title = "") {
+    return MessageBoxA(NULL,content,title, MB_OK | 48);
 };
 
-void print(string content) {
+int MsgBoxA(const char* content, const char* title = "",int icon = 32) {
+    return MessageBoxA(NULL,content,title, MB_OK | icon);
+};
+
+void print(const char* content) {
     using namespace std;
     cout << content << endl;
 };
